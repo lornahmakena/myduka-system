4f0a7bc4bb60be341c0ffcb4e26d17eadfabcc00
lornahmakena <amylornah97@gmail.com>
2025-04-28 13:33:10 +0300

    next commit

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-#import flask
-from flask import Flask
+from flask import Flask,render_template
 
 #instantiate your application - initialization of flask
 app = Flask(__name__)
 
+#routes
 @app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/products')
 def home():
-    pass
+    return render_template("products.html")
+
+@app.route('/sales')
+def about_us():
+    return render_template("sales.html")
+
+@app.route('/dashboard')
+def dashboard():
+    return "Dashbo"
+
+@app.route('/contact_us')
+def contact_us():
+    return "Contact Us"
 
 #run your app
 app.run(debug=True)
